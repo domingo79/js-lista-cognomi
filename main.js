0b16fe33eab5358d4c8cc37b477bbc898637a4a6
domingo79 <andria140779@yahoo.it>
2021-04-28 15:53:50 +0200

    stampa cognomi

---
+++ main.js
@@ -0,0 +1,17 @@
+var cognomi = ['Bianchi', 'Neri', 'Rossi', 'Verdi', 'Gialli'];
+
+//1. chiedi all’utente il cognome
+var cognome_add = prompt('Inserisci il tuo Cognome');
+
+//2. inseriscilo in un array con altri cognomi: ‘Bianchi’, ‘Neri’, ‘Rossi’, ‘Verdi’, ‘Gialli’
+cognomi.push(cognome_add);
+
+// 3. stampa la lista ordinata alfabeticamente
+cognomi = cognomi.sort()
+console.log(cognomi);
+
+// 4. scrivi anche la posizione "umana" (partendo da 1) della lista in cui il nuovo utente si trova
+for (i = 0; i < cognomi.length; i++) {
+    console.log('Posizione ' + (i + 1) + ': Signor. ' + cognomi[i]);
+}
+
