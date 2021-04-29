1b35bdd2a9781ed2ca3d1a13d266f8f8fecea494
domingo79 <andria140779@yahoo.it>
2021-04-29 10:26:55 +0200

    corretto

--- main.js
+++ main.js
@@ -6,16 +6,25 @@ var cognome_add = prompt('Inserisci il tuo Cognome');
 //2. inseriscilo in un array con altri cognomi: ‘Bianchi’, ‘Neri’, ‘Rossi’, ‘Verdi’, ‘Gialli’
 cognomi.push(cognome_add);
 
+// copia del mio array originale
+var CopyCognomi = cognomi.slice();
+
 // 3. stampa la lista ordinata alfabeticamente
-cognomi = cognomi.sort()
+CopyCognomi = CopyCognomi.sort()
+
 //console.log(cognomi);
 var li_id = document.getElementById('lista_cognomi');
 var htmlContent = '';
 
 // 4. scrivi anche la posizione "umana" (partendo da 1) della lista in cui il nuovo utente si trova
-for (var i = 0; i < cognomi.length; i++) {
-    console.log('Posizione ' + (i + 1) + ': Signor, ' + cognomi[i]);
-    htmlContent += '<li>' + cognomi[i] + '</li>'
+for (var i = 0; i < CopyCognomi.length; i++) {
+    console.log('Posizione ' + (i + 1) + ': Signor, ' + CopyCognomi[i]);
+    htmlContent += '<li>' + CopyCognomi[i] + '</li>'
 }
 
 li_id.innerHTML = htmlContent;
+
+// cerca la posizione 
+var pos = CopyCognomi.indexOf(cognome_add) + 1;
+console.log(pos);
+document.getElementById('posizione').innerHTML = pos;
